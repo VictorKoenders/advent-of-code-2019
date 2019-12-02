aa61c375587cca2d3e4dc24be0a1efb99e478d5f
V.Koenders <V.Koenders@gs.ridder.com>
2019-12-02 13:52:10 +0100

    Day 1

---
+++ src/bin/day1.rs
@@ -0,0 +1,47 @@
+extern crate aoc;
+
+pub fn main() {
+    let input = aoc::input!();
+    let mut sum = 0;
+    for num in input.lines().filter_map(|l| l.parse::<isize>().ok()) {
+        let value = calc_fuel_value(num);
+        sum += value;
+    }
+    println!("Part 1: {}", sum);
+
+    let mut sum = 0;
+    for num in input.lines().filter_map(|l| l.parse::<isize>().ok()) {
+        let value = calc_fuel_value_recursive(num);
+        sum += value;
+    }
+    println!("Part 2: {}", sum);
+}
+
+fn calc_fuel_value(input: isize) -> isize {
+    (input / 3) - 2
+}
+
+fn calc_fuel_value_recursive(input: isize) -> isize {
+    let mut result = calc_fuel_value(input);
+    let mut output = 0;
+    while result > 0 {
+        output += result;
+        result = calc_fuel_value(result);
+    }
+    output
+}
+
+#[test]
+fn test_calc_fuel_value() {
+    assert_eq!(2, calc_fuel_value(12));
+    assert_eq!(2, calc_fuel_value(14));
+    assert_eq!(654, calc_fuel_value(1969));
+    assert_eq!(33583, calc_fuel_value(100756));
+}
+
+#[test]
+fn test_calc_fuel_value_recursive() {
+    assert_eq!(2, calc_fuel_value_recursive(12));
+    assert_eq!(966, calc_fuel_value_recursive(1969));
+    assert_eq!(50346, calc_fuel_value_recursive(100756));
+}
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#[macro_export]
+macro_rules! input {
+    () => {{
+        use std::io::Read;
+
+        let file_name = std::file!();
+        let mut file_name = std::path::PathBuf::from(file_name);
+        file_name.set_extension("txt");
+
+        let mut buffer = String::new();
+        std::fs::File::open(&file_name)
+            .unwrap_or_else(|e| panic!("Could not open {:?}: {:?}", file_name, e))
+            .read_to_string(&mut buffer)
+            .unwrap_or_else(|e| panic!("Could not read {:?}: {:?}", file_name, e));
+
+        buffer
+    }};
+}
